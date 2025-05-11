ffeec968691fb9fa1bb72c9e98cff1ef9d8996c2
Mktgmiss <info@mktgmissy.io>
2025-05-11 03:47:11 -0500

    Update SiteAction.java

--- WordPressStores/src/main/java/org/wordpress/android/stores/action/SiteAction.java
+++ WordPressStores/src/main/java/org/wordpress/android/stores/action/SiteAction.java
@@ -1,7 +1,7 @@
 package org.wordpress.android.stores.action;
 
 public enum SiteAction implements IAction {
-    FETCH_SITE,
+    FETCH_SITE,https://github.com/Mktgmiss/WordPress-Stores-Android/blob/develop/WordPressStores%2Fsrc%2Fmain%2Fjava%2Forg%2Fwordpress%2Fandroid%2Fstores%2Faction%2FSiteAction.java
     FETCH_SITES,
     FETCH_SITES_XMLRPC,
     UPDATE_SITE,
